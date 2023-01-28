02deac5c83f63b26372c7faf5cb403b1d597b9b6
Djonatan Rodrigues de Oliveira <101300063+DjhonyOliveira@users.noreply.github.com>
2023-01-27 21:33:48 -0300

    js_finalizado

--- atividade 04/index.js
+++ atividade 04/index.js
@@ -1,11 +1,13 @@
 const nome = prompt("Qual seu nome?")
 let visitou = prompt("Você visitou alguma cidade? (Sim/Não)")
 
-let array = []
+let cidades = []
 
 
 while (visitou === 'Sim') {
   var cidade = prompt("Qual o nome da cidade?")
-  array.push(cidade)
-  visitou = prompt("Você visitou alguma cidade? (Sim/Não)")  
+  cidades.push(cidade)
+  visitou = prompt("Você visitou mais alguma cidade? (Sim/Não)")  
 }
+
+alert(nome + " Visitou " + cidades.length + " Cidades, sendo elas: " + cidades)
