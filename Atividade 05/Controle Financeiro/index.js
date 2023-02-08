ed2762e644fc7a8604b2d6aababe020e21c0472c
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-02-07 21:06:26 -0300

    ajustesJS

--- Atividade 05/Controle Financeiro/index.js
+++ Atividade 05/Controle Financeiro/index.js
@@ -3,24 +3,25 @@ alert("Bem vindo ao controle financeiro!!")
 let dinheiro = prompt("Qual valor você tem disponivel atualmente?")
 let movimentacao = prompt("Você tem: " + dinheiro + "R$ Qual operação deseja realizar?\nAdiciona digite 'a'\nRemover um valor X digite 'b'\nRemover total digite 'c'")
 
-if (movimentacao === "a") {
-    let soma = prompt("Qual valor deseja adicionar?")
-} else if (movimentacao == "b") {
-    let subtracao = prompt("Qual valor deseja subtrair?")
-}
 
-let somaNumber = parseFloat(soma)
 let dinheiroNumber = parseFloat(dinheiro)
-let subtracaoNumber = parseFloat(subtracao)
-
+let dinheiroCalculado = []
 
 do {
     switch (movimentacao) {
         case "a" :
-            alert("Agora voê tem o valor de: " + dinheiroNumber + somaNumber + " R$")
+            let soma = prompt("Qual valor deseja adicionar?")
+            let somaNumber = parseFloat(soma)
+            alert("Agora voê tem o valor de: " + (dinheiroNumber + somaNumber) + " R$")
+            dinheiroCalculado.push(dinheiroNumber + somaNumber)
+            movimentacao = prompt("Você tem: " + dinheiroCalculado + "R$ Qual operação deseja realizar?\nAdiciona digite 'a'\nRemover um valor X digite 'b'\nRemover total digite 'c'")
         break
         case "b" :
-            alert("Agora voê tem o valor de: " + dinheiroNumber + subtracaoNumber + " R$")
+            let subtracao = prompt("Qual valor deseja subtrair?")
+            let subtracaoNumber = parseFloat(subtracao)
+            alert("Agora voê tem o valor de: " + (dinheiroNumber - subtracaoNumber) + " R$")
+            dinheiroCalculado.push(dinheiroNumber + subtracaoNumber)
+            movimentacao = prompt("Você tem: " + dinheiroCalculado + "R$ Qual operação deseja realizar?\nAdiciona digite 'a'\nRemover um valor X digite 'b'\nRemover total digite 'c'")
         break
     }
 } while (movimentacao != "c")
