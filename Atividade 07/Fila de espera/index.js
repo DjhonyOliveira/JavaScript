db0d5ab6db9dfd2ad0e075a6b55a562263aa94e3
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-03-27 21:12:43 -0300

    ajustesJS

--- Atividade 07/Fila de espera/index.js
+++ Atividade 07/Fila de espera/index.js
@@ -2,6 +2,7 @@ const cadastro = document.querySelector(".cadastrar")
 const consultar = document.querySelector(".consultar");
 const consultados = document.querySelector(".consultados");
 const paci = document.querySelector(".paciente")
+const consult = document.querySelector(".paciConsult")
 
 let pacientes = []
 let pacientesConsultados = []
@@ -21,16 +22,24 @@ consultar.addEventListener('click', () =>{
 
     let dadosPaciente = (
         '<div class="content">' +
-            '<div class="pacienteNome">Nome: ' + pacientes[0] + '</div>' +
-            '<div class="idadePaciente">Idade: ' + pacientes[1] + '</div>' +
-            '<div class="pesoPaciente">Peso: ' + pacientes[2] + '</div>' + 
-            '<div class="alturaPaciente">Altura: ' + pacientes[3] + '</div>' +
+            '<div class="pacienteNome">' + 
+                '<p>Nome: ' + pacientes[0] + '</p>' +
+            '</div>' +
+            '<div class="idadePaciente">' +
+                '<p>Idade: ' + pacientes[1] + '</p>' +
+            '</div>' +
+            '<div class="pesoPaciente">' + 
+                '<p>Peso: ' + pacientes[2] + '</p>' + 
+            '</div>' + 
+            '<div class="alturaPaciente">' +
+                '<p>Altura: ' + pacientes[3] + '</p>' + 
+            '</div>' +
         '</div>'
     )
 
 
     if (pacientes.length > 0) {
-        
+
         paci.innerHTML = dadosPaciente
         
         let consultados = {}
@@ -50,13 +59,19 @@ consultar.addEventListener('click', () =>{
    
 
     for ( let i = 1; i <= 4; i++) {
-        pacientes.shift(consultados)
+        pacientes.shift()
     }
 
 })
 
-consultados.addEventListener('click', () =>{
+consultados.addEventListener('click', ()=> {
+    for (let i = 0; i <= pacientesConsultados.length; i++) {
+         var paciConsultados = (
+            '<div class="consultado">' + pacientesConsultados[i] + '</div>'
+        )
+    }
 
+    consult.innerHTML = paciConsultados
 })
 
 
