72ff326d9590f5e3db6f77fa75e116e269924511
DjhonyMaga <djonatandeoliveira@hotmail.com>
2023-04-02 21:14:54 -0300

    objectReturnFront

--- Atividade 07/Fila de espera/index.js
+++ Atividade 07/Fila de espera/index.js
@@ -22,16 +22,17 @@ consultar.addEventListener('click', () =>{
 
     let dadosPaciente = (
         '<div class="content">' +
-            '<div class="pacienteNome">' + 
+            '<h1 class="text-center" style="color: #0d6efd">Proximo a ser consultado:</h1>' +
+            '<div class="pacienteNome text-center mt-5">' + 
                 '<p><b>Nome:</b> ' + pacientes[0] + '</p>' +
             '</div>' +
-            '<div class="idadePaciente">' +
+            '<div class="idadePaciente text-center">' +
                 '<p><b>Idade:</b> ' + pacientes[1] + '</p>' +
             '</div>' +
-            '<div class="pesoPaciente">' + 
+            '<div class="pesoPaciente text-center">' + 
                 '<p><b>Peso:</b> ' + pacientes[2] + '</p>' + 
             '</div>' + 
-            '<div class="alturaPaciente">' +
+            '<div class="alturaPaciente text-center">' +
                 '<p><b>Altura:</b> ' + pacientes[3] + '</p>' + 
             '</div>' +
         '</div>'
@@ -66,19 +67,50 @@ consultar.addEventListener('click', () =>{
 
 consultados.addEventListener('click', ()=> {
 
-        for (let i = 0; i <= pacientesConsultados.length; i++){
-            var paciConsultados = (
-                '<div class="consultado nomeConsultado">' + pacientesConsultados[i].nome + '</div>' +
-                '<div class="consultado idadeConsultado">' + pacientesConsultados[i].idade + '</div>' +
-                '<div class="consultado pesoConsultado">' + pacientesConsultados[i].peso + '</div>' +
-                '<div class="consultado alturaConsultado">' + pacientesConsultados[i].altura + '</div>'
-            )
-        }
+    var nomes = pacientesConsultados.map(function(pessoaNome) {
+        return pessoaNome.nome
+    })
+
+    var idades = pacientesConsultados.map(function(pessoaIdade){
+        return pessoaIdade.idade
+    })
+      
+    var pesos = pacientesConsultados.map(function(pessoaPeso){
+        return pessoaPeso.peso
+    })
+
+    var alturas = pacientesConsultados.map(function(pessoaAltura){
+        return pessoaAltura.altura
+    })
+
+    for (var i = 0; i < nomes.length; i++){
+        var nomePacientes = nomes[i]
+        var idadePacientes = idades[i]
+        var pesoPacientes = pesos[i]
+        var alturaPacientes = alturas[i]
+    }
+
+    var paciConsultados = (
+        '<div class="content">'  +
+            '<h3 class="text-center mt-3" style="color: #0d6efd">Pacientes já consultados:</h3>' +
+            '<div class="consultado nomeConsultado text-center mt-5">' +
+                '<p><b>Nome: </b>' + nomePacientes + '</p>' +
+            '</div>' +
+            '<div class="consultado idadeConsultado text-center">' +
+                '<p><b>Idade: </b>' + idadePacientes + '</p>' +
+            '</div>' +
+            '<div class="consultado pesoConsultado text-center">' +
+                '<p><b>Peso: </b>' + pesoPacientes + '</p>' +
+            '</div>' +
+            '<div class="consultado alturaConsultado text-center">' + 
+                '<p><b>Altura: </b>' +alturaPacientes + '</p>' +
+            '</div>' +
+        '</div>'
+    )
 
     consult.innerHTML = paciConsultados
+
+
 });
-
-
-
 
 
